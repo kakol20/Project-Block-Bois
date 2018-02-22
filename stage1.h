59272eefedeea3af8fe476d161e3e16e43098eb7
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2018-02-22 21:42:59 +0000

    Updated maps
    Compiling currently not working

--- stage1.h
+++ stage1.h
@@ -5,11 +5,11 @@
 //
 //	stage1, 256x256@4, 
 //	+ palette 256 entries, not compressed
-//	+ 4 tiles (t|f|p reduced) not compressed
+//	+ 2 tiles (t|f|p reduced) not compressed
 //	+ regular map (in SBBs), not compressed, 32x32 
-//	Total size: 512 + 128 + 2048 = 2688
+//	Total size: 512 + 64 + 2048 = 2624
 //
-//	Time-stamp: 2018-02-22, 17:00:16
+//	Time-stamp: 2018-02-22, 21:19:38
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -18,8 +18,8 @@
 #ifndef GRIT_STAGE1_H
 #define GRIT_STAGE1_H
 
-#define stage1TilesLen 128
-extern const unsigned short stage1Tiles[64];
+#define stage1TilesLen 64
+extern const unsigned short stage1Tiles[32];
 
 #define stage1MapLen 2048
 extern const unsigned short stage1Map[1024];
